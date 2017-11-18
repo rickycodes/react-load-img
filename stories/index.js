96a6285717e555b02695b4cd2e477f202179f460
Ricky Miller <ricky.miller@gmail.com>
2017-11-18 02:47:43 -0500

    Be consistent

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from '@storybook/react'
 import LoadImage from '../'
 
 storiesOf('LoadImage', module)
