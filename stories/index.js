e8d299412f61cc20c48560cf3c9421dab3711d3a
Ricky Miller <ricky.miller@gmail.com>
2017-11-18 02:11:53 -0500

    Fix entry

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import LoadImage from '../src'
+import LoadImage from '../'
 
 storiesOf('LoadImage', module)
   .add('basic usage', () => {
